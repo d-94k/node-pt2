991fa34368e36508742deaf0e15fc36db7277ad0
d-94k <86233674+d-94k@users.noreply.github.com>
2023-02-03 17:37:43 +0100

    authenticated

--- index.ts
+++ index.ts
@@ -2,12 +2,19 @@ import express, { NextFunction, Request, Response } from "express";
 import { HttpError } from "./models/http-error";
 import { router } from "./router/router";
 import cors from "cors";
+import { initSessionMiddleware } from "./middleware/session";
+import { passport } from "../testnode/middleware/passport";
+import authRoutes from "../testnode/router/auth";
 
 const app = express ();
 
+app.use (initSessionMiddleware ());
+app.use (passport.initialize ());
+app.use (passport.session ());
 app.use(cors());
 app.use (express.json());
 app.use ("/", router);
+app.use ("/auth", authRoutes);
 app.use (() => {throw new HttpError ("page not found", 404)});
 app.use ((error: any, request: Request, response: Response, next: NextFunction) => {
     if (response.headersSent) {
--- router/router.ts
+++ router/router.ts
@@ -2,12 +2,13 @@ import express from "express";
 import { check } from "express-validator";
 import { createUser, deleteUser, getUsers, updateUser, uploadPicture } from "../controllers/user-controllers";
 import { initMulterMiddleware } from "../middleware/multer";
+import { checkAuthorization } from "../middleware/passport";
 
 export const router = express.Router ();
 
 router.get ("/", getUsers);
-router.post ("/", [check("username").not().isEmpty(), check("password").isLength({min: 5, max: 20})], createUser);
-router.put ("/", updateUser);
-router.delete ("/:id", deleteUser);
-router.post ("/:id/photo", initMulterMiddleware().single("photo"), uploadPicture);
+router.post ("/", checkAuthorization, [check("username").not().isEmpty(), check("password").isLength({min: 5, max: 20})], createUser);
+router.put ("/", checkAuthorization, updateUser);
+router.delete ("/:id", checkAuthorization, deleteUser);
+router.post ("/:id/photo", checkAuthorization, initMulterMiddleware().single("photo"), uploadPicture);
 router.use ("/photos", express.static ("uploads"));
